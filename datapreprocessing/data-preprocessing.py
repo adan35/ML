6be0b185945621617b623ff56a4f58e4f3ac633d
adan35 <adanshahzad35@gmail.com>
2022-06-27 03:22:10 +0500

    necessary imports

---
+++ datapreprocessing/data-preprocessing.py
@@ -0,0 +1,16 @@
+#import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#import dataset
+
+#taking care of missing data
+
+#encoding categorical data
+#encoding independent variable
+#encoding dependent variable
+
+#spliting the dataset into the training set and testing set
+
+#feature scaling

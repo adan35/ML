4f8375e733755fc42cc7e8cf7e4648552f4982de
adan35 <adanshahzad35@gmail.com>
2022-07-03 03:22:10 +0500

    modifying param of train_test_split random_state to 0

--- datapreprocessing/data-preprocessing.py
+++ datapreprocessing/data-preprocessing.py
@@ -33,7 +33,7 @@ y = le.fit_transform(y)
 
 #spliting the dataset into the training set and testing set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 # print(X_train)
 # print(X_test)
 # print(y_train)

c5af90874dd5faa49265fbad4c755b334aa7d88e
adan35 <adanshahzad35@gmail.com>
2022-07-16 03:22:10 +0500

    importing dataset

--- regression/multiple_linear_regression/model.py
+++ regression/multiple_linear_regression/model.py
@@ -4,6 +4,9 @@ import matplotlib as plt
 import pandas as pd
 
 # Import Dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
 
 # Encoding categorical data
 
